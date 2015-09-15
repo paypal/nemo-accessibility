6e56f11774f14f568312eeb0cf7356906852a997
Khan <mpnkhan@gmail.com>
2015-09-15 15:55:08 +0530

    Test full and partial page

--- example/dynamicpage.js
+++ example/dynamicpage.js
@@ -3,25 +3,33 @@ var fs = require('fs');
 
 var nemo =  Nemo(process.cwd()+'/example',function(err){
   if(!err) {
+
     nemo.driver.get('http://localhost/test/bs_modal_dynamic/donate.html');
-    console.log('Nemo initialized successfully');
     var options = {
       'priority': ['P1', 'P2', 'P3']
-    };
-    nemo.driver.sleep(1000);
+    };        
+    nemo.accessibility.scan(options).then(function (result) {
+        var file = process.cwd() + '/example/report/entirePage.html';
+        fs.writeFile(file, result,function (err) {
+           console.log('Successfully wrote the file ' + file);
+        });
+    });
+
     var btn= nemo.view._find('css:#btnDonate');    
     btn.click();
     nemo.driver.sleep(2000);
 
+    var options = {
+            'priority': ['P1', 'P2'],
+            'source': 'btnDonate'
+        };
     nemo.accessibility.scan(options).then(function (result) {
-      var file = process.cwd() + '/example/defaultOptions.html';
-      fs.writeFile(file, result, function (err) {
-        if (err) {
-          throw err;
-        }
-        console.log('Successfully wrote the file ' + file);
-      });
+        var file = process.cwd() + '/example/report/scanAnElement.html';
+        fs.writeFile(file, result, function (err) {
+            console.log('Successfully wrote the file ' + file);
+        });
     });
+
     nemo.driver.quit();
   } else {
     console.log(err);
