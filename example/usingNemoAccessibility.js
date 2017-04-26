cfa6338d1048aaf774c671b20b7b4556d92d21b4
Khan <mpnkhan@gmail.com>
2017-04-26 14:37:10 +0530

    Upgrade HTMLCS

--- example/usingNemoAccessibility.js
+++ example/usingNemoAccessibility.js
@@ -5,10 +5,10 @@ var nemo =  Nemo(process.cwd()+'/example',function(err){
     nemo.driver.get('http://www.paypal.com');
     console.log('Nemo initialized successfully');
     var options = {
-      'engine': 'chrome'
+      'engine': 'htmlcs'
     };
     nemo.accessibility.scan(options).then(function (result) {
-      var file = process.cwd() + '/example/report/chrome.html';
+      var file = process.cwd() + '/example/report/htmlcs.html';
       fs.writeFile(file, result, function (err) {
         if (err) {
           throw err;
