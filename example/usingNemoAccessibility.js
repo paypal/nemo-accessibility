797d717010867b5b26eda56aa577d49103f4714b
Khan <mpnkhan@gmail.com>
2018-12-11 15:21:53 +0530

    change json name to accessibility.json

--- example/usingNemoAccessibility.js
+++ example/usingNemoAccessibility.js
@@ -4,15 +4,9 @@ var nemo =  Nemo(process.cwd()+'/example',function(err){
   if(!err) {
     nemo.driver.get('http://www.paypal.com');
     console.log('Nemo initialized successfully');
-    var options = {
-      'engine': 'htmlcs',
-      'project': 'givingfund',
-      'page': 'HomePage'   
-    };
-
-    nemo.accessibility.scan(options).then(function (result) {
+    nemo.accessibility.scan().then(function (result) {
       console.log('result', result);
-      var file = process.cwd() + '/example/report/chrome1.html';
+      var file = process.cwd() + '/example/report/accessibility.json';
       fs.writeFile(file, result, function (err) {
         if (err) {
           throw err;
